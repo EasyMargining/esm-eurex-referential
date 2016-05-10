7addef2e0b0fbef161d28ede17a131c27f0509a4
rjean030116 <robin.jean@ensimag.grenoble-inp.fr>
2016-05-10 13:00:02 +0200

    Add enum (forgotten in previous commit)

---
+++ src/main/java/com/easymargining/domain/enumeration/Exchange.java
@@ -0,0 +1,8 @@
+package com.easymargining.domain.enumeration;
+
+/**
+ * The Exchange enumeration.
+ */
+public enum Exchange {
+    eurex, lse, euronext
+}
---
+++ src/main/java/com/easymargining/domain/enumeration/State.java
@@ -0,0 +1,8 @@
+package com.easymargining.domain.enumeration;
+
+/**
+ * The State enumeration.
+ */
+public enum State {
+    live, expirated
+}
